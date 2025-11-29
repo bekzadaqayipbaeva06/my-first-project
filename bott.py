18b6daf664587b5ad611a247cb886fce62b4536b
bekzadaqayipbaeva06 <bekzadaqayipbaeva4@gmail.com>
2025-11-29 09:07:22 +0500

    4 ta faylni qo'shdim

--- bott.py
+++ bott.py
@@ -1,21 +1,39 @@
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram import types
+from aiogram.filters import CommandStart, Command
+from config import token
 import asyncio
 
-TOKEN = ""
-my_bot = Bot(token=TOKEN)
+my_bot = Bot(token=str(token))
 dp = Dispatcher()
 
 
+@dp.message(F.photo)
+async def handle_any_photo_message(message: types.Message):
+    await message.copy_to(
+        chat_id=message.chat.id,
+    )
+
+
 @dp.message(CommandStart())
-async def handle_start(message):
-    await message.answer("hi") 
-    
+async def handle_start(message: types.Message):
+    print(message.from_user.id)
+    await message.answer(
+        f"Добро  пожаловать, {message.from_user.full_name}",
+    )
+
+
+@dp.message(Command("info", prefix="!/"))
+async def hanle_info(message: types.Message):
+    await message.answer(
+        text="Это тестовый бот для изучения aiogram",
+    )
+
 
 async def main():
     print("I am starting ...")
     await dp.start_polling(my_bot)
 
 
-#if __name__ == "__main__":
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
